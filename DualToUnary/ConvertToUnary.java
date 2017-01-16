5bb35ce4dd3181bec10f7870aafd63b10d7493d0
maltewo <mawor@gymnasium-neustadt.de>
2017-01-16 22:02:10 +0100

    Eingabefunktion hinzugefügt

--- DualToUnary/ConvertToUnary.java
+++ DualToUnary/ConvertToUnary.java
@@ -5,19 +5,22 @@ import java.util.Scanner;
 
 class ConvertToUnary	{
 	
-	static String binaerzahl = ("110");
+	static String binaerzahl;// = ("110");
+	public static int dezimal;
 	//int dezimal;
 	
-	/*public static void binaerzahl() throws IOException	{
+	public static void eingabe() throws IOException	{
     InputStreamReader isr = new InputStreamReader(System.in);
     BufferedReader br = new BufferedReader(isr);
-    System.out.print("Bitte Binaerzahl eingeben: ");
+    System.out.print("Bitte Binärzahl eingeben: ");
     binaerzahl = br.readLine();
-    }    */
+	dezimal = Integer.parseInt(binaerzahl, 2);  //Hier wird aus dem Binärstring ein Dezimal-Integer erzeugt
+    }    
 	
-	int dezimal = Integer.parseInt(binaerzahl, 2);  //Hier wird aus dem Binärstring ein Dezimal-Integer erzeugt
-	
+
+//Die Methode "Umrechnung" rechnet den Dezimal-Intger in eine unäre Zahl um
 	public void umrechnung()	{
+		System.out.println("Die binäre Zahl " + binaerzahl + " lautet im Unärsystem: ");
 		for (int i=1; i<=dezimal; i++)	{
 			System.out.print(1);
 		}
@@ -26,6 +29,3 @@ class ConvertToUnary	{
 	
 }
 
-
-
-// System.out.println(Integer.toBinaryString(23));
--- DualToUnary/Main.java
+++ DualToUnary/Main.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.Scanner;
+
 
 class Main	{
 	
@@ -6,9 +8,8 @@ class Main	{
 	public static void main(String[] args) throws IOException	{
 			
 		ConvertToUnary convert = new ConvertToUnary();  //Anlegen eines Objektes der Klasse ConvertToUnary
-		//convert.binaerzahl();
+		convert.eingabe();
 		convert.umrechnung();
-		//System.out.println(Integer.toString(0b101010));
 		}
 	
 } 
