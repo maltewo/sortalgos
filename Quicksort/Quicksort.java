8a48febf95a32df96610e3ea73e75ac2939576e3
Malte Müller <malte-m@gmx.net>
2017-01-17 21:21:59 +0100

    Added funktions and implemented exchange()

--- Quicksort/Quicksort.java
+++ Quicksort/Quicksort.java
@@ -1,5 +1,20 @@
 class Quicksort{
 	public static void main(String[] args){
-		
+
+	}
+
+	//Die eigentliche Tauschfunktion
+	public static void sort(int[] a, int l, int r){
+
+	}
+	//Teilt den übergenen Bereich und gibt das Pivotelement zurück
+	public static int partition(int[] a, int l, int r){
+
+	}
+
+	public static void exchange(int[] a, int x, int y){
+		int tmp = a[x];
+		a[x] = a[y];
+		a[y] = tmp;
 	}
 }
