01183c311ff2bbe6895d8e0faa0414342f75d4e0
maltewo <mawor@gymnasium-neustadt.de>
2017-01-19 09:50:40 +0100

    schöner gemacht

--- BubbleSort_Alternative/BubbleSort2.java
+++ BubbleSort_Alternative/BubbleSort2.java
@@ -8,14 +8,14 @@ public class BubbleSort2	{
 	public int[] sort()	{
 		int k;
 		for (int i = 0; i < zahl.length-1; i++)	{
-			if (zahl[i] < zahl[i+1])	{
-				continue;
+			if (zahl[i] > zahl[i+1])	{
+				k = zahl[i];
+				zahl[i] = zahl[i+1];
+				zahl[i+1] = k;
+				ausgabe();
+				sort();	
 			}
-			k = zahl[i];
-			zahl[i] = zahl[i+1];
-			zahl[i+1] = k;
-			ausgabe();
-			sort();
+			
 		}
 		return zahl;
 		
